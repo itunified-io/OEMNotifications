10baa82392f3c3f79bfaec9201094055fe406d5a
itunified-buecheleb <buecheleb@itunified.de>
2025-01-17 08:57:40 +0100

    fix: multiple recipients comma seperated

--- OEMNotification.sh
+++ OEMNotification.sh
@@ -98,8 +98,8 @@ send_email() {
 
     # Split the recipients by semicolon and pass them as individual arguments to mailx
     log "DEBUG" "recipients: $recipients"
-    # Split the recipients by semicolon and create a space-separated string
-    RECIPIENT_LIST=$(echo "$recipients" | tr ';' ' ')
+    # Split the recipients by semicolon and create a comma-separated string
+    RECIPIENT_LIST=$(echo "$recipients" | tr ';' ',')
 
     # Send the email using mailx
     printf "%b" "$body" | mailx -S smtp="$smtp_server:$smtp_port" -S from="$smtp_sender" -s "$SUB1" $RECIPIENT_LIST
